2acd7940e37f9db817109ac8cd8e0303f8a2c0ea
gecmanow <gecmanow@gmail.com>
2024-05-06 19:30:18 +0700

    add callback functions

--- bot.php
+++ bot.php
@@ -292,7 +292,7 @@ switch ($data){
             $keyboard['reply_markup']['inline_keyboard'][$i][0]['callback_data'] = $p['telegram_id'];
         }
 
-        $response = $keyboardStaffDirectSales;
+        $response = $keyboard;
         $response['chat_id'] = $chat_id_in;
         $response['text'] = 'Выберите сотрудника:';
 
