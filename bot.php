0891bde82a67471a0bfbdf69a697fa8e0acf9862
gecmanow <gecmanow@gmail.com>
2024-05-07 01:07:28 +0700

    edit query

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ $token = $_ENV['TOKEN'];
 
 $db_conn = mysqli_connect($host, $user, $password, $db_name);
 echo '<pre>' . print_r($db_conn, true) . '</pre>';
-$query = "SELECT `name`, `telegram_id` FROM `users` WHERE `departament` = 'Прямые продажи' OR 'Прямые продажи/Проектные продажи'";
+$query = "SELECT name, telegram_id FROM users WHERE departament = 'Прямые продажи' OR 'Прямые продажи/Проектные продажи'";
 
 $result = mysqli_query($db_conn, $query);
 $people = [];
